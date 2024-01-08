cc1358a5163cf6d1bce1a43ef427cbdd5942efa3
amkamir82 <amk_amir82@yahoo.com>
2024-01-08 15:09:41 +0330

    complete setup VPN

--- Codes/main.py
+++ Codes/main.py
@@ -9,6 +9,7 @@ private_key_path = config.PRIVATE_KEY_PATH
 
 
 def init_ssh_client():
+    print("Creating ssh client...")
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     my_key = paramiko.RSAKey.from_private_key_file(private_key_path)
@@ -16,9 +17,38 @@ def init_ssh_client():
     return client
 
 
-def scp_files_to_server(ssh_client):
+def scp_files_to_server(client):
+    print("Scp config files to server...")
     files = [config.V2RAY_CONFIG_FILE_PATH, config.DOCKER_COMPOSE_FILE_PATH]
     remote_path = "/root/"
     with SCPClient(client.get_transport()) as scp:
         for file in files:
             scp.put(file, remote_path)
+
+
+def install_docker_compose(client):
+    print("Install docker-compose on server...")
+    try:
+        stdin, stdout, stderr = client.exec_command('apt install docker-compose -y')
+        output = stdout.read()
+        print(output.decode('utf-8'))
+    except:
+        print("error while installing docker-compose")
+
+
+def up_docker_compose(client):
+    print("Run v2ray docker-compose file...")
+    try:
+        stdin, stdout, stderr = client.exec_command('docker-compose up -d')
+        output = stdout.read()
+        print(output.decode('utf-8'))
+    except:
+        print("error while installing docker-compose")
+
+
+if __name__ == "__main__":
+    client = init_ssh_client()
+    scp_files_to_server(client)
+    install_docker_compose(client)
+    up_docker_compose(client)
+    client.close()
